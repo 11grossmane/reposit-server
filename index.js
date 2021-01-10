54669b8876f8c7d56eb3c664f2be398f82456e6c
Eric Grossman <eric.josh.grossman@gmail.com>
2021-01-10 02:24:15 -0500

    sfd

--- index.js
+++ index.js
@@ -1,16 +1,21 @@
 const express = require("express");
+const jwt = require("jwt-express");
 const app = express();
 const port = 9000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.get("/creds", async (req, res) => {
-    // if (res.getHeader('')){
-    //   res.status(401)
-    //   return
-    // }
+// app.use((req, res, next) => {
+//     const auth = req.headers.authorization;
+//     const decoded = jwtDecode(auth.split(' ')[1])
+//     if ()
+//     next();
+// });
+
+app.get("/creds", jwt({ secret: "floppydisk" }), async (req, res) => {
     console.log(process.env);
-    res.send({ id: process.env.ID, sec: process.env.SEC });
+    if (!req.user.admin) res.sendStatus(401);
+    else res.send({ id: process.env.ID, sec: process.env.SEC });
 });
 
 app.listen(process.env.PORT, () => {
