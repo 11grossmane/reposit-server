11259b2e667373129d33c1e013ac3fbdabadb96c
Eric Grossman <eric.josh.grossman@gmail.com>
2021-01-10 02:28:57 -0500

    sdf

--- index.js
+++ index.js
@@ -5,17 +5,15 @@ const port = 9000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-// app.use((req, res, next) => {
-//     const auth = req.headers.authorization;
-//     const decoded = jwtDecode(auth.split(' ')[1])
-//     if ()
-//     next();
-// });
+app.use((req, res, next) => {
+    const auth = req.headers.authorization;
+    const decoded = jwtDecode(auth.split(" ")[1]);
+    if (decoded.app == "reposit") res.sendStatus(401);
+    else next();
+});
 
-app.get("/creds", jwt({ secret: "floppydisk" }), async (req, res) => {
-    console.log(process.env);
-    if (!req.user.admin) res.sendStatus(401);
-    else res.send({ id: process.env.ID, sec: process.env.SEC });
+app.get("/creds", async (req, res) => {
+    res.send({ id: process.env.ID, sec: process.env.SEC });
 });
 
 app.listen(process.env.PORT, () => {
